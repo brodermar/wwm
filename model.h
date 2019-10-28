9f9fabae5da0b88a91307017585af2b343fed0b5
MarkB <mark.broderius@yahoo.de>
2019-10-28 19:45:55 +0100

    added some comments

--- model.h
+++ model.h
@@ -8,7 +8,7 @@
 #include <game.h>
 
 /**
- * @brief The Model class
+ * @brief The Model class represents the data of the complete program.
  */
 class Model : public GameObservable
 {
@@ -16,68 +16,71 @@ class Model : public GameObservable
 private:
 
     /**
-     * @brief activeGame
+     * @brief activeGame the current active game, can be nullptr
      */
     Game* activeGame;
 
     /**
-     * @brief running
+     * @brief running true, if the program is running, false else
      */
     bool running;
 
     /**
-     * @brief mutex
+     * @brief mutex a std::mutex object
      */
     std::mutex mutex;
 
 public:
 
     /**
-     * @brief EXIT_EXP
+     * @brief EXIT_EXP a control statement which can be send to the controller
+     * to exit the program
      */
     static const std::string EXIT_EXP;
 
     /**
-     * @brief QUIT_GAME_EXP
+     * @brief QUIT_GAME_EXP a control statement which can be send to the controller
+     * to quit the current running game
      */
     static const std::string QUIT_GAME_EXP;
 
     /**
-     * @brief START_GAME_EXP
+     * @brief START_GAME_EXP a control statement whicht can be send to the controller
+     * to start a new game, possibly an already running game will be terminated
      */
     static const std::string START_GAME_EXP;
 
     /**
-     * @brief Model
+     * @brief Model constructs a new model
      */
     Model();
 
     ~Model();
 
     /**
-     * @brief getCurrentGame
-     * @return
+     * @brief getCurrentGame return the current active game, can be nullptr
+     * @return the current active game or nullptr
      */
     Game* getCurrentGame();
 
     /**
-     * @brief quitGame
+     * @brief quitGame terminated the current game
      */
     void quitGame();
 
     /**
-     * @brief startNewGame
+     * @brief startNewGame starts a new game
      */
     void startNewGame();
 
     /**
-     * @brief isProgramRunning
-     * @return
+     * @brief isProgramRunning checks if the program is still running
+     * @return true, if the program is running, false else
      */
     bool isProgramRunning();
 
     /**
-     * @brief terminateProgram
+     * @brief terminateProgram terminates the program
      */
     void terminateProgram();
 
--- question.h
+++ question.h
@@ -7,7 +7,7 @@
 #include <answer.h>
 
 /**
- * @brief The Question class
+ * @brief The Question class represents a question of a game.
  */
 class Question
 {
@@ -15,96 +15,96 @@ class Question
 private:
 
     /**
-     * @brief question
+     * @brief question the questions string
      */
     std::string question;
 
     /**
-     * @brief answerA
+     * @brief answerA the first answer
      */
     Answer* answerA;
 
     /**
-     * @brief answerB
+     * @brief answerB the seconde answer
      */
     Answer* answerB;
 
     /**
-     * @brief answerC
+     * @brief answerC the third answer
      */
     Answer* answerC;
 
     /**
-     * @brief answerD
+     * @brief answerD the fourth answer
      */
     Answer* answerD;
 
     /**
-     * @brief rightAnswer
+     * @brief rightAnswer a pointer to the right answer
      */
     Answer* rightAnswer;
 
 public:
 
     /**
-     * @brief Question
-     * @param question
-     * @param answerA
-     * @param answerB
-     * @param answerC
-     * @param answerD
-     * @param rightAnwser
+     * @brief Question constructs a new question
+     * @param question the question string
+     * @param answerA the first answer
+     * @param answerB the second answer
+     * @param answerC the third answer
+     * @param answerD the fourth answer
+     * @param rightAnwser a pointer to the right answer
      */
     Question(std::string question, std::string answerA, std::string answerB, std::string answerC, std::string answerD, char rightAnwser);
 
     ~Question();
 
     /**
-     * @brief getQuestion
-     * @return
+     * @brief getQuestion returns the questions
+     * @return the question string
      */
     std::string getQuestion();
 
     /**
-     * @brief getAnswerA
-     * @return
+     * @brief getAnswerA returns the first answer
+     * @return the first answer
      */
     Answer* getAnswerA();
 
     /**
-     * @brief getAnswerB
-     * @return
+     * @brief getAnswerB returns the second answer
+     * @return the second answer
      */
     Answer* getAnswerB();
 
     /**
-     * @brief getAnswerC
-     * @return
+     * @brief getAnswerC returns the third answer
+     * @return the thrid answer
      */
     Answer* getAnswerC();
 
     /**
-     * @brief getAnswerD
-     * @return
+     * @brief getAnswerD returns the fourth answer
+     * @return the fourth answer
      */
     Answer* getAnswerD();
 
     /**
-     * @brief getRightAnswer
-     * @return
+     * @brief getRightAnswer return a pointer to the right answer
+     * @return a pointer to the right answer
      */
     Answer* getRightAnswer();
 
     /**
-     * @brief evaluateAnswer
-     * @param answer
-     * @return
+     * @brief evaluateAnswer evaluates a given answer string to the question
+     * @param answer a answer string
+     * @return true, if answer matches the right answer, false else
      */
     bool evaluateAnswer(std::string answer);
 
     /**
-     * @brief pullNewQuestion
-     * @return
+     * @brief pullNewQuestion created and returns a new question
+     * @return a new question
      */
     static Question* pullNewQuestion();
 
